6bdb81208af08da609ad1d40500be5efd9178d3b
BaronPipistron <baronpipistron@mail.ru>
2024-08-28 13:48:50 +0300

    Lecture 2

---
+++ lectures_code/lecture-2/1_3_scopes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// 1.3. Declarations, defenitions and scopes.
+
+int a = 5;
+
+void f(int x);
+
+class C;
+struct S;
+enum E;
+union U;
+
+namespace N {
+    int x;
+}
+
+namespace N {
+    int y;
+}
+
+int main() {
+
+}
